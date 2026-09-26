6eeeae13062a7d24873cce2ea10c1df661c804f4
ArmandosLP <155646008+ArmandosLP@users.noreply.github.com>
2026-09-26 03:23:16 +0200

    Hotfix 1.4.2
    Fixed Apotheosis badge

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/YogDzewa.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/YogDzewa.java
@@ -538,7 +538,7 @@ public class YogDzewa extends Mob {
 
 		GameScene.bossSlain();
         
-        if (Statistics.qualifiedForBossChallengeBadge){
+        if (Statistics.qualifiedForApotheosis){
             Badges.validateApotheosis();
         }
 
